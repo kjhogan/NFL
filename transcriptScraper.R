b636c809069b346212b98e88f415d51521362e9d
kjhogan <kevinjohnhogan@gmail.com>
2018-03-29 10:49:31 -0400

    Update transcriptScraper.R

--- transcriptScraper.R
+++ transcriptScraper.R
@@ -23,3 +23,27 @@ transcriptScraper <- function() {
   transcriptdf <- data.frame(transcriptlist, linklist, stringsAsFactors = FALSE)
   return(transcriptdf)
 }
+
+
+
+ get_QB_NFL_Data <- function(page, season = "2018"){
+  Sys.sleep(2)
+  #url <- paste0("https://www.pro-football-reference.com/play-index/draft-finder.cgi?request=1&year_min=1990&year_max=2017&draft_slot_min=1&draft_slot_max=500&pick_type=overall&pos%5B%5D=qb&conference=any&show=p&order_by=default")
+  #download.file(url,"qbslit.html")
+  linkbase <- read_html("qbslit.html") %>% html_nodes("table tbody a") %>% html_attr('href')
+  linkname <- read_html("qbslit.html") %>% html_nodes("table tbody a") %>% html_text()
+  link_logic <- linkbase %>% grepl("^/players/",.)
+  linkbase <- linkbase[link_logic]
+  linkname <- linkname[link_logic]
+  links <- paste0("https://pro-football-reference.com", linkbase)
+  return(linkname)
+ }
+ 
+ get_NFL_Passing <- function(link, name) {
+   download.file(link, "qbpage.html")
+   passing_table <- read_html("qbpage.html") %>% html_nodes("#passing") %>% html_table() %>% .[[1]] %>% as.data.frame()
+   names(passing_table)[25] <- "Sk_Yds"
+   passing_table$Player <- name
+   passing_table <- passing_table %>% filter(Year == "Career")
+   return(passing_table)
+ }
